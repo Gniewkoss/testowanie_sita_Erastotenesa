f6315a3253893fb2e72c45570110bc4c1a85d44f
Gorbkos <gniewkoscielak@gmail.com>
2023-10-12 14:19:37 +0200

    Added new tests and new code in main.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-def sito(number: int):
-    primes = []
-    for i in range(2, number + 1):
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            primes.append(i)
-    return primes
+def sieve_of_eratosthenes(n):
+    is_prime = [True] * (n+1)
+    is_prime[0] = is_prime[1] = False
+    p = 2
+    while p**2 <= n:
+        if is_prime[p]:
+            for i in range(p**2, n+1, p):
+                is_prime[i] = False
+        p += 1
+    return [i for i in range(2, n+1) if is_prime[i]]
 
-print(sito(10))
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def process_prime_numbers(primes):
+    for num in primes:
+        if is_prime(num):
+            print(num)
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
+    primes = sieve_of_eratosthenes(n)
+    process_prime_numbers(primes)
--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import pytest
-from main import sito
+from main import sieve_of_eratosthenes
+from main import is_prime
+from main import process_prime_numbers
+
+@pytest.fixture
+def inputs():
+    return [sieve_of_eratosthenes(i) for i in range(2, 1000)]
+
+@pytest.fixture
+def not_prime_inputs():
+    return [i for i in range(2, 1000) if i not in sieve_of_eratosthenes(i)]
+
+def test_sieve_is_prime(inputs):
+    for primes_list in inputs:
+        for num in primes_list:
+            assert is_prime(num)
 
 
-from main import sito
-
-def test_sito_with_10():
-    result = sito(10)
-    assert result == [2, 3, 5, 7]
-
-def test_sito_with_20():
-    result = sito(20)
-    assert result ==[2, 3, 5, 7, 11, 13, 17, 19]
-
-
-#aasfafasfa
+def test_is_prime_false(not_prime_inputs):
+    for num in not_prime_inputs:
+        assert is_prime(num) == False
